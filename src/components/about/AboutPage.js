604f71ecb84e6afa94f0efa971f3e5e92d4bf92c
oylcn <omeryalcin@outlook.com>
2021-11-26 10:57:48 +0300

    changed aboutPage

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,7 +5,7 @@ const AboutPage = () => (
     <h2>About</h2>
     <p>
       This app uses React, Redux, React Router, and many other helpful
-      libraries.
+      libraries. TEst
     </p>
   </div>
 );
